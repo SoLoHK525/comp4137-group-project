deb97517eac43cc8383203508ce7fbf8c55f499b
hi-0707 <elizamakkt@gmail.com>
2022-04-05 12:48:45 +0800

    Transaction Structure
    Added Transaction Structure

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Logger } from '@nestjs/common';
 import { BlockService } from "../block/block.service";
+import {signature, Transaction, TxIn, TxOut} from "../transaction/transaction.interface";
+import { SHA256 } from 'crypto-js';
 
 @Injectable()
 export class TransactionService {
+  private readonly logger = new Logger(TransactionService.name);
+
   constructor(private readonly blockService: BlockService) {}
 
   onApplicationBootstrap() {
     // When the application starts
   }
+
+  makeTransaction(addrs: string[], amount: number): boolean {
+    return false;
+  }
+
+  broadcast(transaction: Transaction): boolean{
+    //network level -> broadcasting transaction to all nodes
+    return false;
+  }
+
 }
